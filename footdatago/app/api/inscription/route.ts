8ebeaec166410e657102674e743b20f84dd68484
YacineBoukliff <yacine69800@live.fr>
2025-02-05 09:03:07 +0100

    Changement du name (pseudo) pour qu'il soit unique et changement de la route pour ajouter cette exception

--- footdatago/app/api/inscription/route.ts
+++ footdatago/app/api/inscription/route.ts
@@ -7,45 +7,49 @@ import { createElement } from "react";
 import { sendEmail } from "@/app/utils/email";
 
 export async function POST(request: NextRequest) {
-    
-  const { email, password, name  } = await request.json();
+  const { email, password, name } = await request.json();
 
   const emailElement = createElement(EmailBienvenue, { name });
-  const validation = SchemaInscription.safeParse({ email, password ,  name });
+  const validation = SchemaInscription.safeParse({ email, password, name });
   if (!validation.success) {
     return NextResponse.json(
-        { message: validation.error.errors[0].message }, 
-        { status: 400 }
+      { message: validation.error.errors[0].message },
+      { status: 400 }
     );
-}
+  }
 
-  const user = await prisma.user.findUnique({
+  const emailutilisateur = await prisma.user.findUnique({
     where: { email },
   });
 
-  if (user)
+  if (emailutilisateur)
     return NextResponse.json("Cet email est déja utilisé", { status: 400 });
+
+  const nomutilisateur = await prisma.user.findUnique({
+    where: { name },
+  });
+
+  if (nomutilisateur)
+    return NextResponse.json("Ce pseudo est déja utilisé ", { status: 400 });
 
   const motdepasse = await bcrypt.hash(password, 10);
 
-   const result = await sendEmail({
-        to: email,
-        subject: "Bienvenue!",
-        emailComponent: emailElement,
-      });
+  const result = await sendEmail({
+    to: email,
+    subject: "Bienvenue!",
+    emailComponent: emailElement,
+  });
 
   const nouvelutilisateur = await prisma.user.create({
     data: {
       email,
       password: motdepasse,
-      name, 
-      
+      name,
     },
   });
 
   return NextResponse.json({
-    name : nouvelutilisateur.name ,
-    message : "Inscription réussie "
-  
+    name: nouvelutilisateur.name,
+    message: "Inscription réussie ",
   });
 }
