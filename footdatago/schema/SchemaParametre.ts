1becfa82ca3c675ebffca6f95dd00437e6ba51a7
YacineBoukliff <yacine69800@live.fr>
2025-02-05 19:14:18 +0100

    Ajout de tout les schéma pour la section paramètre et ajout de la route post et patch pour le changement de pseudo plus qu'a gérer les providers

--- footdatago/schema/SchemaParametre.ts
+++ footdatago/schema/SchemaParametre.ts
@@ -7,7 +7,34 @@ export const SchemaChangementPseudo = z.object({
     .min(6, { message: "Votre pseudo doit avoir au moins 6 caractères" })
     .max(35, { message: "Votre pseudo doit avoir au maximum 35 caractères" }),
 
-    codeverification: z.string({ message: "Vous devez mettre un code" })
+  codeverification: z
+    .string({ message: "Vous devez mettre un code" })
     .min(1, { message: "Vous devez mettre un code" })
     .length(6, { message: "Le code doit contenir exactement 6 caractères" }),
 });
+
+export const SchemaChangementEmail = z.object({
+  email: z
+    .string({ message: "Vous devez mettre un email" })
+    .email({ message: "Vous devez mettre un email valide" }),
+
+  codeverification: z
+    .string({ message: "Vous devez mettre un code" })
+    .min(1, { message: "Vous devez mettre un code" })
+    .length(6, { message: "Le code doit contenir exactement 6 caractères" }),
+});
+
+export const SchemaChangementMotDePasse = z.object({
+  motdepasse: z
+    .string({ message: "Vous devez mettre un pseudo" })
+    .min(1, { message: "Vous devez mettre un pseudo" })
+    .min(6, { message: "Votre pseudo doit avoir au moins 6 caractères" })
+    .max(35, { message: "Votre pseudo doit avoir au maximum 35 caractères" }),
+
+  codeverification: z
+    .string({ message: "Vous devez mettre un code" })
+    .min(1, { message: "Vous devez mettre un code" })
+    .length(6, { message: "Le code doit contenir exactement 6 caractères" }),
+});
+
+
