5971872a401a0aec7a18bd8555a4d39b1b857954
YacineBoukliff <yacine69800@live.fr>
2025-02-08 09:06:54 +0100

    Refacto des formulaire avec les servers actions et création du form inscription

---
+++ footdatago/app/(actions)/InscriptionAction.tsx
@@ -0,0 +1,68 @@
+"use server"
+
+import bcrypt from "bcryptjs";
+import { z } from "zod";
+import SchemaInscription from "@/schema/SchemaInscription";
+import { prisma } from "@/prisma";
+import EmailBienvenue from "@/app/(emails)/EmailBievenue";
+import { createElement } from "react";
+import { sendEmail } from "@/app/utils/email";
+
+type Schema = z.infer<typeof SchemaInscription>;
+
+export async function inscriptionAction(data: Schema) {
+  try {
+    const emailutilisateur = await prisma.user.findUnique({
+      where: { email: data.email }
+    });
+    
+    if (emailutilisateur) {
+      return {
+        success: false,
+        error: "Cet email est déja utilisé"
+      };
+    }
+
+    const nomutilisateur = await prisma.user.findUnique({
+      where: { name: data.name }
+    });
+    
+    if (nomutilisateur) {
+      return {
+        success: false,
+        error: "Ce pseudo est déja utilisé"
+      };
+    }
+
+    const motdepasse = await bcrypt.hash(data.password, 10);
+
+    const emailElement = createElement(EmailBienvenue, { name: data.name });
+    await sendEmail({
+      to: data.email,
+      subject: "Bienvenue!",
+      emailComponent: emailElement,
+    });
+
+    const nouvelutilisateur = await prisma.user.create({
+      data: {
+        email: data.email,
+        password: motdepasse,
+        name: data.name,
+      }
+    });
+
+    return {
+      success: true,
+      data: {
+        name: nouvelutilisateur.name,
+        message: "Inscription réussie"
+      }
+    };
+
+  } catch (error) {
+    return {
+      success: false,
+      error: "Une erreur est survenue lors de l'inscription"
+    };
+  }
+}
