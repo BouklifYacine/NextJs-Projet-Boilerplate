717ff6128db89dd187f4bce876c11564c2bd2a76
YacineBoukliff <yacine69800@live.fr>
2025-02-09 11:07:31 +0100

    Ajout de la page qui render si l'utilisateur est bien connecté et a bien un abonnement

---
+++ footdatago/app/pro/page.tsx
@@ -0,0 +1,26 @@
+import { auth } from '@/auth'
+import { prisma } from '@/prisma'
+import { redirect } from 'next/navigation'
+import React from 'react'
+// Page pour tester si l'utilisateur a bien un abonnement 
+const ProPage = async () => {
+    const session = await auth()
+    if(!session) return redirect("/")
+
+        const sessiondetails = session.user?.name
+      const sessiond = JSON.stringify(sessiondetails)
+
+      const utilisateur = await prisma.user.findUnique({
+        where : {id : session.user?.id}
+      })
+
+      if(utilisateur?.plan === "free") return redirect('/')
+  return (
+    <div>  {sessiond && <p>Vous etes connecté </p> }
+    {utilisateur?.plan === "pro" && <p>Vous etes abonné a notre offre </p>}
+    
+    </div>
+  )
+}
+
+export default ProPage
