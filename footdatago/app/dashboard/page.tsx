ae20273d7cdd843d7476e999e747611feb0d8ef3
YacineBoukliff <yacine69800@live.fr>
2025-02-16 13:52:00 +0100

    Protection de la route dashboard de deux manière première si la session n'existe pas donc pas connecté et si il est pas admin

--- footdatago/app/dashboard/page.tsx
+++ footdatago/app/dashboard/page.tsx
@@ -7,17 +7,30 @@ import {
 } from "./(actions)/Dashboard";
 import Header from "@/components/header";
 import { TableauDeBordClient } from ".//Tableau";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
+import { prisma } from "@/prisma";
 
 const TableauDeBord = async () => {
-
   const utilisateurs = await GetUtilisateurs();
   const totalUtilisateurs = await getTotalUtilisateurs();
   const totalAbonnements = await getTotalAbonnement();
   const statsAbonnements = await getAbonnementStats();
   const totalRevenus = statsAbonnements.total.revenus;
-  const MRR = Number(statsAbonnements.total.mrr)
-  const RevenusParUtilisateurs =  (Number(totalRevenus) / totalUtilisateurs ).toFixed(2)
+  const MRR = Number(statsAbonnements.total.mrr);
+  const RevenusParUtilisateurs = (
+    Number(totalRevenus) / totalUtilisateurs
+  ).toFixed(2);
 
+  const session = await auth();
+  if (!session?.user?.id) redirect("/");
+  
+  const user = await prisma.user.findUnique({ 
+    where: { id: session.user.id },
+    select: { role: true } 
+  });
+  
+  if (!user || user.role !== "Admin") redirect("/");
 
   const statistiques = {
     totalUtilisateurs,
@@ -29,7 +42,12 @@ const TableauDeBord = async () => {
   return (
     <>
       <Header />
-      <TableauDeBordClient utilisateurs={utilisateurs} statistiques={statistiques} MRR={MRR} RevenusParUtilisateurs={RevenusParUtilisateurs} />
+      <TableauDeBordClient
+        utilisateurs={utilisateurs}
+        statistiques={statistiques}
+        MRR={MRR}
+        RevenusParUtilisateurs={RevenusParUtilisateurs}
+      />
     </>
   );
 };
