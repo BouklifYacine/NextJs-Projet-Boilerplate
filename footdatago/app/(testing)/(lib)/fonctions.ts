a5ee1531f0f031c99fb6505c2e6505cc433d40fe
YacineBoukliff <yacine69800@live.fr>
2025-02-20 12:54:54 +0100

    On passe la suite des tests

--- footdatago/app/(testing)/(lib)/fonctions.ts
+++ footdatago/app/(testing)/(lib)/fonctions.ts
@@ -8,3 +8,10 @@ export function Fizzbuzz(nombre: number) {
   else if (nombre % 5 === 0) return "Buzz";
   return "Guez"
 }
+
+export function CalculerMoyenne(tableau : number[]){
+
+  const totaltableau =  tableau.reduce((somme , valeuractuelle) => somme + valeuractuelle , 0)
+  const moyenne = totaltableau / tableau.length
+  return tableau.length === 0 ? NaN : moyenne
+}
--- footdatago/app/(testing)/test.test.ts
+++ footdatago/app/(testing)/test.test.ts
@@ -1,5 +1,5 @@
 import { describe, test, expect } from "vitest";
-import { Fizzbuzz, PlusGrand } from "./(lib)/fonctions";
+import { CalculerMoyenne, Fizzbuzz, PlusGrand } from "./(lib)/fonctions";
 
 describe("PlusGrand", () => {
   test("Doit retourner le premier argument s'il est plus grand ", () => {
@@ -8,22 +8,31 @@ describe("PlusGrand", () => {
   });
 });
 
-describe("FizzBuzz" , () => {
-    test("Doit retourner FizzBuzz" , () => {
-        const resultat = Fizzbuzz(15)
-        expect(resultat).toBe("FizzBuzz")
-    })
+describe("FizzBuzz", () => {
+  test("Doit retourner FizzBuzz", () => {
+    const resultat = Fizzbuzz(15);
+    expect(resultat).toBe("FizzBuzz");
+  });
 
-    test("Doit retourner Buzz ", () => {
-        expect(Fizzbuzz(5)).toBe("Buzz")
-    })
+  test("Doit retourner Buzz ", () => {
+    expect(Fizzbuzz(5)).toBe("Buzz");
+  });
 
-    test("Doit retourner Fizz ", () => {
-        expect(Fizzbuzz(9)).toBe("Fizz")
-    })
+  test("Doit retourner Fizz ", () => {
+    expect(Fizzbuzz(9)).toBe("Fizz");
+  });
 
-    test("Doit retourner Guez ", () => {
-        expect(Fizzbuzz(7)).toBe("Guez")
-    })
-    
-})
+  test("Doit retourner Guez ", () => {
+    expect(Fizzbuzz(7)).toBe("Guez");
+  });
+});
+
+describe("CalculerMoyenne", () => {
+  test("Doit retourner moyenne", () => {
+    expect(CalculerMoyenne([])).toBe(NaN);
+  });
+
+  test("Doit retourner la moyenne du tableau avec une valeur" , () => {
+    expect(CalculerMoyenne([5,15])).toBe(10)
+  })
+});
