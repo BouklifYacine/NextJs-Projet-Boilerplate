622d98d2c1a70bb9bed8fc5bf449db02d11995d7
YacineBoukliff <yacine69800@live.fr>
2025-02-20 11:14:18 +0100

    Test réussi du FizzBuzz

--- footdatago/app/(testing)/test.test.ts
+++ footdatago/app/(testing)/test.test.ts
@@ -4,14 +4,26 @@ import { Fizzbuzz, PlusGrand } from "./(lib)/fonctions";
 describe("PlusGrand", () => {
   test("Doit retourner le premier argument s'il est plus grand ", () => {
     const resultat = PlusGrand(2, 1);
-
     expect(resultat).toBe(2);
   });
 });
 
 describe("FizzBuzz" , () => {
-    test("Doit retourner FizzBuzz ou fizz ou Buzz" , () => {
-        const resultat = Fizzbuzz(5)
-        expect(resultat).toBe("Buzz")
+    test("Doit retourner FizzBuzz" , () => {
+        const resultat = Fizzbuzz(15)
+        expect(resultat).toBe("FizzBuzz")
     })
+
+    test("Doit retourner Buzz ", () => {
+        expect(Fizzbuzz(5)).toBe("Buzz")
+    })
+
+    test("Doit retourner Fizz ", () => {
+        expect(Fizzbuzz(9)).toBe("Fizz")
+    })
+
+    test("Doit retourner Guez ", () => {
+        expect(Fizzbuzz(7)).toBe("Guez")
+    })
+    
 })
