dcdf0dd1d65cc4afe02ac27076e84faf71eecf23
YacineBoukliff <yacine69800@live.fr>
2025-02-23 10:44:19 +0100

    Suppression du dossier test et fix de la server action car ca redirigeait a chaque fois

--- Devgo/app/(actions)/AdminAction.ts
+++ Devgo/app/(actions)/AdminAction.ts
@@ -1,18 +1,17 @@
 "use server";
 import { auth } from "@/auth";
 import { prisma } from "@/prisma";
-import { redirect } from "next/navigation";
 
 export async function AdminAction() {
   const session = await auth();
   const sessionID = session?.user?.id;
 
-  if (!sessionID) redirect("/");
+  if (!sessionID) throw new Error("Vous n'avez pas de session ID")
 
   const utilisateur = await prisma.user.findUnique({
     where: { id: sessionID },
   });
-  if (utilisateur?.role !== "Admin") redirect("/");
+  if (utilisateur?.role !== "Admin") throw new Error("Vous n'etes pas admin")
 
   return { success: true, Admin: utilisateur.role === "Admin" };
 }
