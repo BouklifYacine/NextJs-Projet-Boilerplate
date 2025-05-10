18849ba24ad24fa8ca85b1ade701a007247d01f8
YacineBoukliff <yacine69800@live.fr>
2025-05-10 14:34:54 +0200

    Ajout des sections select et radioGroup

--- Devgo/app/salut/page.tsx
+++ Devgo/app/salut/page.tsx
@@ -3,14 +3,7 @@
 import React from "react";
 import { authClient } from "@/lib/auth-client";
 import { DarkMode } from "@/components/BoutonDarkMode/DarkMode";
-import { TestimonialsListe } from "@/components/Testimonials/Testimonials";
-import { Chat } from "@/components/Chat/Chat";
-import { Pricing2Demo, PricingBasic } from "@/components/Pricing/Pricing1";
-import { BoutonSupprimerAnimation } from "@/components/Boutons/BoutonSupprimerAnimation";
-import { BoutonSupprimer } from "@/components/Boutons/BoutonSupprimer";
-import { FormulaireInscription } from "@/components/Formulaires/FormulaireInscription";
-;
-
+import { RadioGroupBlock } from "@/components/RadioGroup/RadioGroupBlock";
 
 const Salut = () => {
   const { data: session } = authClient.useSession();
@@ -20,15 +13,9 @@ const Salut = () => {
   const src =
     "https://cdn.vox-cdn.com/thumbor/r0U59Lx7DOSI2Z_F7WLnzcbQfuU=/1400x1400/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/24953495/1698708349.jpg";
 
-    
-
-  return (
- 
-  
-
-<FormulaireInscription></FormulaireInscription>
-   
-  );
+  return <>
+  <RadioGroupBlock></RadioGroupBlock>
+  </>;
 };
 
 export default Salut;
