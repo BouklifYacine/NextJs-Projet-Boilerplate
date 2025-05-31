e0013b66332f8e035264e7361037540e245a8a52
YacineBoukliff <yacine69800@live.fr>
2025-05-31 12:18:02 +0200

    Test redeploy automatique

--- app/connexion/page.tsx
+++ app/connexion/page.tsx
@@ -9,7 +9,7 @@ export default async function ConnexionPage() {
     <div className="min-h-screen  flex items-center justify-center">
       <div className="bg-white p-8 rounded-lg shadow-lg w-96">
         <h2 className="text-2xl font-bold text-center text-black mb-6">
-          Se connecterrr HG
+          Se connecter
         </h2>
         <ClientConnexionFormulaire />
       </div>
