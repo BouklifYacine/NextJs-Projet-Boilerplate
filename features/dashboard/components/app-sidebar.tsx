f43bbc8396edfd458328e8ff80796950474cec20
YacineBoukliff <yacine69800@live.fr>
2025-06-07 14:45:30 +0200

    Fix d'un props inutile

--- features/dashboard/components/app-sidebar.tsx
+++ features/dashboard/components/app-sidebar.tsx
@@ -26,11 +26,7 @@ import { NavUser } from "./nav-user";
 
 // This is sample data.
 const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
+
   teams: [
     {
       name: "Acme Inc",
@@ -164,7 +160,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <NavMain items={data.navMain} />
       </SidebarContent>
       <SidebarFooter>
-        <NavUser user={data.user} />
+        <NavUser  />
       </SidebarFooter>
       <SidebarRail />
     </Sidebar>
