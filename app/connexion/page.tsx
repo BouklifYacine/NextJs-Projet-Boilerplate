070aa8eda7e75ff257ee97e133ec518e7b08e3b2
YacineBoukliff <yacine69800@live.fr>
2025-12-25 15:25:43 +0100

    Fix de la page de connexion pour une meilleur UI et sémantique HTML pour accessibilité

--- app/connexion/page.tsx
+++ app/connexion/page.tsx
@@ -3,20 +3,24 @@ import { ConnexionFormulaire } from "../../features/connexion/components/Connexi
 import { GalleryVerticalEnd } from "lucide-react";
 
 export default async function ConnexionPage() {
-
   await MiddlewareUtilisateurConnecte();
-  
+
   return (
-  <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
+    <main className="min-h-svh bg-muted flex flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm space-y-8">
+        <header className="flex flex-col items-center gap-4 text-center">
+          <div className="bg-primary text-primary-foreground flex size-10 items-center justify-center rounded-xl shadow-sm">
+            <GalleryVerticalEnd className="size-6" />{" "}
+            {/* TODO: Remplacer par un logo */}
           </div>
-          Devgogo
-        </a>
-        <ConnexionFormulaire />
+          <h1 className="text-huge font-bold tracking-tight">Boilerplate</h1>{" "}
+          {/* TODO: Remplacer par un titre */}
+        </header>
+
+        <section className="bg-background rounded-2xl shadow-xs">
+          <ConnexionFormulaire />
+        </section>
       </div>
-    </div>
+    </main>
   );
 }
